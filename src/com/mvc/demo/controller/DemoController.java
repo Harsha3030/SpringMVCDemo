f49ef1fdcca21f85011a83a14055284ae398e7bf
Harsha <nallagatla.harsha@gmail.com>
2018-06-27 20:30:09 -0500

    myfirstcommit

---
+++ src/com/mvc/demo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.mvc.demo.controller;
+
+@Controller
+@RequestMapping("/hello")
+public class DemoController {
+	
+	@RequestMapping(method=REQUESTMETHOD.GET)
+	public static demo1(Model Model) {
+		model
+	}
+
+	
+
+}
